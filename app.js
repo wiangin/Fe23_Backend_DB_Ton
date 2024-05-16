f67a0a1e201f306203473b050a26a126699b7953
Ton <146097352+wiangin@users.noreply.github.com>
2024-05-16 10:53:54 +0200

    commit all

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 //in dev run nodemon with for instant reload
 //add requiered library 
-const express = require('express'); //must be installed with npm
-const ejs = require('ejs'); //must be installed with npm
-const db = require('./db.js'); // Import the database module
-const bodyParser = require('body-parser');//must be installed with npm
+
+// const express = require('express'); //must be installed with npm
+// const ejs = require('ejs'); //must be installed with npm
+// const db = require('./db.js'); // Import the database module
+// const bodyParser = require('body-parser');//must be installed with npm
+
+import express from 'express';
+import ejs from "ejs";
+import db from "./db.js";
+import bodyParser from 'body-parser';
 
 //create variable representing express
 const app = express();
 
 //set public folder for static web pages
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 //set dynamic web pages, set views and engine
 app.set('view engine', 'ejs');
@@ -19,33 +25,78 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 ////////////////Routing
 
+//////// Reuest med endpoint * / * \\\\\\\\\\
+//////// Reuest med endpoint * / * \\\\\\\\\\
+//////// Reuest med endpoint * / * \\\\\\\\\\
 app.get('/', async (req, res) => {
     //res.send("hello World");//serves index.html
-    const pageTitle = "Dynamic webpage";
-    const sql = 'SELECT * FROM students';
-    const dbData = await db.query(sql);
-    console.log(dbData);
-    res.render('index', {pageTitle, dbData} );
+    const pageTitle = "School Database";
+    const sql = 'SHOW TABLES'
+    const [dbData] = await db.query(sql);
+    // console.log(dbData);
+    res.render('index', {pageTitle, dbData});
 });
 
-app.post('/submit', async (req, res) => {
-    try {
-      console.log(req.body);
-      //const {plant1, plant2} = req.body;
-        // Example query to fetch users from 'users' table
-        const sql = 'SELECT * FROM plants';
-        const plants = await db.query(sql);
-        console.log(plants);
-        res.render('dbList',{plants});
-        //res.json(plants); // Return fetched data as JSON response
-       //res.render('dbList', {plants});
-      } catch (error) {
-        console.error('Error executing query:', error);
-        res.status(500).send('Internal Server Error');
-      }
+let currentTable;  
+
+// Form input\\
+app.post('/', async (req, res) => {
+  //res.send("hello World");//serves index.html
+  console.log(req.body);
+  const tableName = req.body; // with Post need request from body
+
+  const pageTitle = `Table name = ${tableName.table_name}`; 
+
+  // Query to Mysql \\
+  const sql = `Select * FROM ${tableName.table_name} `;
+  currentTable = tableName.table_name;
+  const [dbData] = await db.query(sql);
+  // console.log(dbData);
+
+  const sql2 = `DESCRIBE ${tableName.table_name}`;
+  const [dbDataHeaders] = await db.query(sql2);
+  // console.log(dbDataHeaders);
+
+   // TO render on webpage\\
+  res.render('index', {pageTitle, dbData, dbDataHeaders});
 });
 
+//////// Reuest med endpoint * removeData * \\\\\\\\\\
+//////// Reuest med endpoint * removeData * \\\\\\\\\\
+//////// Reuest med endpoint * removeData * \\\\\\\\\\
 
+app.get('/removeData', async (req, res) => {
+  //res.send("hello World");//serves index.html
+  const pageTitle = "Remove Data";
+  
+  const sql = `SELECT * FROM ${currentTable}`;
+  const [dbData] = await db.query(sql);
+  // console.log(dbData);
+
+  const sql2 = `DESCRIBE ${currentTable}`;
+  const [dbDataHeaders] = await db.query(sql2);
+
+  res.render('removeData', {pageTitle, dbData, dbDataHeaders, currentTable});
+});
+ 
+app.post('/removeData', async (req, res) => {
+  console.log(req.body);
+  const pageTitle = "Remove Data";
+  const inputData = req.body;
+
+  const sqlDeleteQuery = `DELETE FROM ${currentTable} WHERE student_id = ${inputData.id}`;
+  const deleteQuery = await db.query(sqlDeleteQuery);
+  console.log(deleteQuery);
+
+  const sql = `SELECT * FROM ${currentTable}`;
+  const [dbData] = await db.query(sql);
+
+  const sql2 = `DESCRIBE ${currentTable}`;
+  const [dbDataHeaders] = await db.query(sql2);
+
+  res.render("removeData", {pageTitle, dbData, dbDataHeaders, currentTable});
+ 
+})
 
 //server configuration
 const port = 3000;
--- db.js
+++ db.js
@@ -1,26 +1,32 @@
-const mysql = require("mysql2");//must be installed with npm
+// const mysql = require("mysql2");//must be installed with npm
+
+import mysql from "mysql2/promise.js";
 
 // Create a pool of database connections
 const pool = mysql.createPool({
     host: 'localhost',
-    port: '3306',
+    port: '3308',
     user: 'root',
     password: '',
-    database: 'gritacademy',
+    database: 'skoldatabas',
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    rowsAsArray: true
   });
-  // Export a function to execute SQL queries
-module.exports = {
-    query: (sql, values) => {
-      return new Promise((resolve, reject) => {
-        pool.query(sql, values, (err, results) => {
-          if (err) {
-            return reject(err);
-          }
-          return resolve(results);
-        });
-      });
-    }
-  };
+
+// Export a function to execute SQL queries
+// module.exports = {
+//     query: (sql, values) => {
+//       return new Promise((resolve, reject) => {
+//         pool.query(sql, values, (err, results) => {
+//           if (err) {
+//             return reject(err);
+//           }
+//           return resolve(results);
+//         });
+//       });
+//     }
+//   };
+
+export default pool;
