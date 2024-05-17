8521b4ddab92ccadb6e2e7e689b82a008d7b33bd
Ton <146097352+wiangin@users.noreply.github.com>
2024-05-17 09:00:28 +0200

    Commit

--- app.js
+++ app.js
@@ -42,18 +42,18 @@ let currentTable;
 // Form input\\
 app.post('/', async (req, res) => {
   //res.send("hello World");//serves index.html
-  console.log(req.body);
+  // console.log(req.body);
   const tableName = req.body; // with Post need request from body
 
-  const pageTitle = `Table name = ${tableName.table_name}`; 
+  const pageTitle = `Table name = ${tableName.userInput}`; 
 
   // Query to Mysql \\
-  const sql = `Select * FROM ${tableName.table_name} `;
-  currentTable = tableName.table_name;
+  const sql = `Select * FROM ${tableName.userInput} `;
+  currentTable = tableName.userInput;
   const [dbData] = await db.query(sql);
-  // console.log(dbData);
+  console.log(dbData);
 
-  const sql2 = `DESCRIBE ${tableName.table_name}`;
+  const sql2 = `DESCRIBE ${tableName.userInput}`;
   const [dbDataHeaders] = await db.query(sql2);
   // console.log(dbDataHeaders);
 
@@ -80,13 +80,13 @@ app.get('/removeData', async (req, res) => {
 });
  
 app.post('/removeData', async (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   const pageTitle = "Remove Data";
-  const inputData = req.body;
+  const tableName = req.body;
 
-  const sqlDeleteQuery = `DELETE FROM ${currentTable} WHERE student_id = ${inputData.id}`;
+  const sqlDeleteQuery = `DELETE FROM ${currentTable} WHERE id = ${tableName.id}`;
   const deleteQuery = await db.query(sqlDeleteQuery);
-  console.log(deleteQuery);
+  // console.log(deleteQuery);
 
   const sql = `SELECT * FROM ${currentTable}`;
   const [dbData] = await db.query(sql);
@@ -97,6 +97,50 @@ app.post('/removeData', async (req, res) => {
   res.render("removeData", {pageTitle, dbData, dbDataHeaders, currentTable});
  
 })
+
+//////// Reuest med endpoint * addData * \\\\\\\\\\
+//////// Reuest med endpoint * addData * \\\\\\\\\\
+//////// Reuest med endpoint * addData * \\\\\\\\\\
+
+app.get('/addData', async (req, res) => {
+  //res.send("hello World");//serves index.html
+  const pageTitle = "Add Data";
+  
+  const sql = `SELECT * FROM ${currentTable}`;
+  const [dbData] = await db.query(sql);
+  // console.log(dbData);
+
+  const sql2 = `DESCRIBE ${currentTable}`;
+  const [dbDataHeaders] = await db.query(sql2);
+
+  res.render('addData', {pageTitle, dbData, dbDataHeaders, currentTable});
+});
+
+app.post('/addData', async (req, res) => {
+  //res.send("hello World");//serves index.html
+  // console.log( typeof req.body.student_name);
+  const tableName = req.body; // with Post need request from body
+  const age = parseInt(tableName.student_age);
+  const classId = parseInt(tableName.class_id);
+
+  const pageTitle = `Table name = ${currentTable}`; 
+
+  // Query to Mysql \\
+
+  const insertQuery = `INSERT INTO ${currentTable}(student_name, student_age, class_id) VALUES ("${tableName.student_name}", ${age}, ${classId})`;
+  const addDbData = await db.query(insertQuery);
+  console.log(addDbData);
+
+  const sql = `SELECT * FROM ${currentTable}`;
+  const [dbData] = await db.query(sql);
+  
+  const sql2 = `DESCRIBE ${currentTable}`;
+  const [dbDataHeaders] = await db.query(sql2);
+  // console.log(dbDataHeaders);
+
+   // TO render on webpage\\
+  res.render('addData', {pageTitle, dbData, dbDataHeaders, currentTable});
+});
 
 //server configuration
 const port = 3000;
